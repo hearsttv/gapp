ea111ad3d4487c8e5b65571de688a6aae1c9648c
Nathan Smith <nsmith@hearst.com>
2015-07-26 13:03:49 -0700

    added some documentation to the callback interface

--- app.go
+++ app.go
@@ -19,18 +19,27 @@ type HandlerMapping struct {
 
 // Gapp defines the callback interface that a webservice must implement
 type Gapp interface {
+	// LoadConfig callback allows the app to load the app config. Optionally save the config as a resource for use outside of callbacks
 	LoadConfig() gappconfig.Config
+	// ConfigureLogging callback allows the app to do any custom log configuration (i.e. based on environmental config)
 	ConfigureLogging(conf gappconfig.Config)
+	// InitResources callback is where the app would set up DB connections, start internal goroutine daemons, etc.
 	InitResources(conf gappconfig.Config)
+	// SetHandlers callback allows the app to set the webservice's handlers
 	SetHandlers(conf gappconfig.Config) []HandlerMapping
+	// SetNotFoundHandler callback allows the app to optionally set a 404 handler
 	SetNotFoundHandler(conf gappconfig.Config) http.Handler
+	// SetMiddleware callback allows the app to set Negroni middleware handlers. Gapp comes with some handy middleware you can use.
 	SetMiddleware(conf gappconfig.Config) []negroni.Handler
+	// GetServerConf callback prompts the app for the host and port to listen on. The final return value is the length of time to allow handlers to finish on stop before shutting down the service.
 	GetServerConf(conf gappconfig.Config) (host string, port int, gracefulTimeout time.Duration)
+	// HandleStart callback is fired right before the service starts listening
 	HandleStart(host string, port int)
+	// HandleStopped callback is fired after the app has stopped listening. Teardown code should go here.
 	HandleStopped()
 }
 
-// Run runs a Gapp app object, using its callbacks to configure and fire events
+// Run runs a Gapp app object, using its callbacks to configure and fire events. Run blocks until the service is stopped.
 func Run(app Gapp) {
 	config := app.LoadConfig()
 	app.ConfigureLogging(config)
